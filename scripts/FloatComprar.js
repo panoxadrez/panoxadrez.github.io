0ed8f2cc33d83dc6de8cdd4cf49184b5bb13b3d1
panoxadrez <44067928+panoxadrez@users.noreply.github.com>
2018-10-13 21:32:07 -0300

    Update scripts/FloatComprar.js

--- scripts/FloatComprar.js
+++ scripts/FloatComprar.js
@@ -1,7 +1,18 @@
 window.onscroll = function(){
    var altura = window.pageYOffset || document.documentElement.scrollTop
    if( altura > 40 ) {
-		$(".FloatComprar").css({"position": "fixed", "button": "0px"});}
+		$(".FloatComprar").css({
+			"position": "fixed",
+			"button": "0px",
+			"overflow": "visible",
+			"width": "100vw",
+			"height": "initial"});}
    else{
-		$(".FloatComprar").css({"position": "relative", "button": "0"});}
+		$(".FloatComprar").css({
+			"position": "relative", 
+			"button": "0", 
+			"overflow": "hidden",
+			"width": "0px",
+			"height": "0px"
+		});}
 }
