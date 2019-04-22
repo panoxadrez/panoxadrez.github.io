a511b62554c7d69e22310ac752e26fc2128375d9
panoxadrez <gabriel.loch@countrycraft.com.br>
2019-04-22 17:17:56 -0300

    Update carousel_init.js

--- scripts/carousel_init.js
+++ scripts/carousel_init.js
@@ -1,6 +1,7 @@
-var $carousel = $('.carousel').flickity({
-  "wrapAround": true,
-  "draggable": true,
-  "prevNextButtons": false,
-  "pageDots": false
+$carousel = $('.carousel').flickity({
+    "wrapAround": true,
+    "draggable": true,
+    "prevNextButtons": false,
+    "pageDots": false
 });
+//a
