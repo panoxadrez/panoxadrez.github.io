149ef5850aef587ec836ed16f2ef7a00695fc0b4
panoxadrez <gabriel.loch@countrycraft.com.br>
2019-04-22 17:00:39 -0300

    a

--- scripts/carousel_init.js
+++ scripts/carousel_init.js
@@ -1,25 +1,3 @@
-// external js: flickity.pkgd.js
-
-var flkty = new Flickity( '.carousel', {
+var flkty = new Flickity.onload( ".carousel", {
   autoPlay: 1000
-});
-
-var playButton = document.querySelector('.play-button');
-playButton.addEventListener( 'click', function() {
-  flkty.playPlayer();
-});
-
-var stopButton = document.querySelector('.stop-button');
-stopButton.addEventListener( 'click', function() {
-  flkty.stopPlayer();
-});
-
-var pauseButton = document.querySelector('.pause-button');
-pauseButton.addEventListener( 'click', function() {
-  flkty.pausePlayer();
-});
-
-var unpauseButton = document.querySelector('.unpause-button');
-unpauseButton.addEventListener( 'click', function() {
-  flkty.unpausePlayer();
 });
