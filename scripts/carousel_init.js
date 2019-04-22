e5f4456b1b887de7d3ef334a9bcbbcba9fd792fb
panoxadrez <gabriel.loch@countrycraft.com.br>
2019-04-22 16:45:23 -0300

    a

--- scripts/carousel_init.js
+++ scripts/carousel_init.js
@@ -1,9 +1,7 @@
-$('.main-carousel').flickity(
-        {
-            // options
-            cellAlign: 'left',
-            contain: true,
-            draggable: false
-        }
-);
-     
+var flkty = new Flickity('.main-carousel', {
+    // options
+    cellAlign: 'left',
+    contain: true,
+    draggable: false
+    
+});
