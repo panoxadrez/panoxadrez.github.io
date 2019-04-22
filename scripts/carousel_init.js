3c506eaed054a1dcfd4adf62d80f42e8e97454fe
panoxadrez <gabriel.loch@countrycraft.com.br>
2019-04-22 17:44:44 -0300

    Update carousel_init.js

--- scripts/carousel_init.js
+++ scripts/carousel_init.js
@@ -2,8 +2,8 @@ var carouselHome = document.querySelector('.main-carousel');
 var flkty = new Flickity(carouselHome, {
     // options
     cellAlign: 'left',
-    contain: true,
-    draggable: false,
-    prevNextButtons: false,
-    pageDots: false
+    contain: 'true',
+    draggable: 'false',
+    prevNextButtons: 'false',
+    pageDots: 'false'
 });
