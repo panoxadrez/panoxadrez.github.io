f56e6edec5a864149f2daae3c2238d46ffef98d9
panoxadrez <44067928+panoxadrez@users.noreply.github.com>
2019-04-25 14:49:32 -0300

    a

--- scripts/carousel_init.js
+++
@@ -1,9 +0,0 @@
-var carouselHome = document.querySelector('.carousel');
-var flkty = new Flickity(carouselHome, {
-    // options
-    cellAlign: 'left',
-    contain: true,
-    draggable: false,
-    prevNextButtons: false,
-    pageDots: false
-});
