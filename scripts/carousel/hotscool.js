33ebf6d039a57fea6a9d5cf7aa4b4fb86bc9b8ba
panoxadrez <44067928+panoxadrez@users.noreply.github.com>
2019-05-16 13:58:25 -0300

    a

--- scripts/carousel/hotscool.js
+++ scripts/carousel/hotscool.js
@@ -3,5 +3,5 @@ var carouselHeaderHome = new Flickity(document.querySelector('.carousel-home'), 
     draggable: '>1',
     autoPlay: 15000,
     wrapAround: true,
-
+    prevNextButtons: false,
 });
