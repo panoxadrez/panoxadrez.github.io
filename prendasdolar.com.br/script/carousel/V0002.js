839d723721ed9f2ecd9f21c741a002f06ac29f48
Gabriel <grl.loch@gmail.com>
2019-06-12 16:02:29 -0300

    a

--- prendasdolar.com.br/script/carousel/V0002.js
+++ prendasdolar.com.br/script/carousel/V0002.js
@@ -27,3 +27,21 @@ var carouselCursoDestaque = new Flickity(document.querySelector('.carousel.curso
     setGallerySize: false,
     contain: true
 });
+
+//var autoPlayDepoimentos = 10500;
+var autoPlayDepoimentos = false;
+
+
+
+var carouselDepoimentos1 = new Flickity(document.querySelector('.depoimentos-home1'), {
+    adaptiveHeight: true,autoPlay: autoPlayDepoimentos,lazyLoad: 2,wrapAround: true,pageDots: false,draggable: false,prevNextButtons: false,
+});
+var carouselDepoimentos2 = new Flickity(document.querySelector('.depoimentos-home2'), {
+    autoPlay: autoPlayDepoimentos,lazyLoad: 2,wrapAround: true,pageDots: false,draggable: false,prevNextButtons: false,
+});
+var carouselDepoimentos3 = new Flickity(document.querySelector('.depoimentos-home3'), {
+    autoPlay: autoPlayDepoimentos,lazyLoad: 2,wrapAround: true,pageDots: false,draggable: false,prevNextButtons: false,
+});
+var carouselDepoimentos4 = new Flickity(document.querySelector('.depoimentos-home4'), {
+    autoPlay: autoPlayDepoimentos,lazyLoad: 2,wrapAround: true,pageDots: false,draggable: false,prevNextButtons: false,
+});
