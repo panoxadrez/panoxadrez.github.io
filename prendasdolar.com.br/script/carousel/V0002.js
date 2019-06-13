d73a1d9e62d3303e96c8e2efe3cb2ada468c4a97
Gabriel <grl.loch@gmail.com>
2019-06-13 16:11:24 -0300

    1

--- prendasdolar.com.br/script/carousel/V0002.js
+++ prendasdolar.com.br/script/carousel/V0002.js
@@ -1,3 +1,30 @@
+var $carousel = $('.carousel').flickity({
+    autoPlay: 1000
+});
+$carousel.flickity('playPlayer');
+
+
+
+
+
+
+//var autoPlayDepoimentos = 10500;
+var autoPlayDepoimentos = 3500;
+
+var width = parseInt(screen.width);
+var height = parseInt(screen.height);
+var rightToLeftStatus = false;
+
+if (width > height) {
+    rightToLeftStatus: true;
+}
+
+
+
+
+
+
+
 var carouselHome = new Flickity(document.querySelector('.carousel-home'), {
     draggable: '>1',
     lazyLoad: true,
@@ -5,7 +32,7 @@ var carouselHome = new Flickity(document.querySelector('.carousel-home'), {
     wrapAround: true,
     prevNextButtons: false,
     cellSelector: '.carousel-cell',
-   // initialIndex: 1
+    // initialIndex: 1
 });
 
 var carouselCurso = new Flickity(document.querySelector('.carousel-header'), {
@@ -28,27 +55,15 @@ var carouselCursoDestaque = new Flickity(document.querySelector('.carousel.curso
     contain: true
 });
 
-//var autoPlayDepoimentos = 10500;
-var autoPlayDepoimentos = 30500;
-
-var width = parseInt(screen.width);
-var height = parseInt(screen.height);
-var rightToLeftStatus = false;
-
-if (width > height){
-rightToLeftStatus: true;    
-}
-
-
 var carouselDepoimentos1 = new Flickity(document.querySelector('.depoimentos-home1'), {
-    autoPlay: autoPlayDepoimentos,lazyLoad: 2,wrapAround: true,pageDots: false,draggable: false,prevNextButtons: false, pauseAutoPlayOnHover: false, hash: true
+    autoPlay: autoPlayDepoimentos, lazyLoad: 2, wrapAround: true, pageDots: false, draggable: false, prevNextButtons: false, pauseAutoPlayOnHover: false, hash: true
 });
 var carouselDepoimentos2 = new Flickity(document.querySelector('.depoimentos-home2'), {
-    autoPlay: autoPlayDepoimentos,lazyLoad: 2,wrapAround: true,pageDots: false,draggable: false,prevNextButtons: false, pauseAutoPlayOnHover: false, hash: true
+    autoPlay: autoPlayDepoimentos, lazyLoad: 2, wrapAround: true, pageDots: false, draggable: false, prevNextButtons: false, pauseAutoPlayOnHover: false, hash: true
 });
 var carouselDepoimentos3 = new Flickity(document.querySelector('.depoimentos-home3'), {
-    autoPlay: autoPlayDepoimentos,lazyLoad: 2,wrapAround: true,pageDots: false,draggable: false,prevNextButtons: false, pauseAutoPlayOnHover: false, hash: true
+    autoPlay: autoPlayDepoimentos, lazyLoad: 2, wrapAround: true, pageDots: false, draggable: false, prevNextButtons: false, pauseAutoPlayOnHover: false, hash: true
 });
 var carouselDepoimentos4 = new Flickity(document.querySelector('.depoimentos-home4'), {
-    autoPlay: autoPlayDepoimentos,lazyLoad: 2,wrapAround: true,pageDots: false,draggable: false,prevNextButtons: false, pauseAutoPlayOnHover: false, hash: true
+    autoPlay: autoPlayDepoimentos, lazyLoad: 2, wrapAround: true, pageDots: false, draggable: false, prevNextButtons: false, pauseAutoPlayOnHover: false, hash: true
 });
