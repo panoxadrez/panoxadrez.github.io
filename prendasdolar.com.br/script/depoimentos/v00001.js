9c057e18bc7c3efd434c27ef30c02920ae0c3218
Gabriel <grl.loch@gmail.com>
2019-06-17 16:22:08 -0300

    a

--- prendasdolar.com.br/script/depoimentos/v00001.js
+++ prendasdolar.com.br/script/depoimentos/v00001.js
@@ -1,14 +1,3 @@
-function random(number) {
-    return Math.floor(Math.random(number) * depoimentosLength);
-}
-function insertDepoimento(classe, position) {
-    document.querySelector(".depoimentos " + classe + " .foto").innerHTML = "<img src='" + depoimentosList[position][2] + "'>";
-    document.querySelector(".depoimentos " + classe + " .nome").innerHTML = "<h5>" + depoimentosList[position][0] + "</h5> <span>" + depoimentosList[position][1] + "</span>";
-    document.querySelector(".depoimentos " + classe + " .texto").innerHTML = "<p>" + depoimentosList[position][3] + "</p>";
-}
-function excluiDepoimento(depoimento) {
-    delete depoimentosList[depoimento];
-}
 var depoimentosList = [
     ['DEPOIMENTO1', 'curso de criacao', 'http://www.iconninja.com/files/904/113/246/puppy-small-pet-dog-shape-icon.png', 'lalala'],
     ['DEPOIMENTO2', 'curso de criacao', 'http://www.iconninja.com/files/904/113/246/puppy-small-pet-dog-shape-icon.png', 'lalala'],
@@ -17,30 +6,40 @@ var depoimentosList = [
     ['DEPOIMENTO5', 'curso de criacao', 'http://www.iconninja.com/files/904/113/246/puppy-small-pet-dog-shape-icon.png', 'lalala']
 ];
 
+var depoimentosLength = depoimentosList.length;
+
 var depoimento1 = ".depoimento1";
 var depoimento2 = ".depoimento2";
 var depoimento3 = ".depoimento3";
 var depoimento4 = ".depoimento4";
 
+function random(number) {
+    return Math.floor(Math.random(number) * depoimentosLength);
+}
 
-var depoimentosLength = depoimentosList.length;
-var randomNumber = random(depoimentosLength);
+function selecionarDepoimento(length) {
+    var i = 0;
+    var counter = 1;
+    while (i <= 0) {
+        var randomNumber = random(length);
+        console.log("random number"+randomNumber + " / loop " + counter);
+        if (depoimentosList[randomNumber] != null) {
+            i = 1
+        }
+        counter++;
+    }
+    return randomNumber;
+}
+function insertDepoimento(classe, position) {
+    document.querySelector(".depoimentos " + classe + " .foto").innerHTML = "<img src='" + depoimentosList[position][2] + "'>";
+    document.querySelector(".depoimentos " + classe + " .nome").innerHTML = "<h5>" + depoimentosList[position][0] + "</h5> <span>" + depoimentosList[position][1] + "</span>";
+    document.querySelector(".depoimentos " + classe + " .texto").innerHTML = "<p>" + depoimentosList[position][3] + "</p>";
+    delete depoimentosList[position];
+}
 
 
-console.log("depoimentosLength = " + depoimentosLength + " / randomNumber = " + randomNumber)
-var insert1 = insertDepoimento(depoimento1, randomNumber);
+var insert1 = insertDepoimento(depoimento1, selecionarDepoimento());
+var insert2 = insertDepoimento(depoimento2, selecionarDepoimento());
+var insert3 = insertDepoimento(depoimento3, selecionarDepoimento());
+var insert4 = insertDepoimento(depoimento4, selecionarDepoimento());
 
-
-delete depoimentosList[randomNumber];
-randomNumber = random(depoimentosLength);
-console.log("depoimentosLength = " + depoimentosLength + " / randomNumber = " + randomNumber)
-
-
-depoimentosLength = depoimentosList.length;randomNumber = random(depoimentosLength);
-var insert2 = insertDepoimento(depoimento2, randomNumber);
-
-depoimentosLength = depoimentosList.length;randomNumber = random(depoimentosLength);
-var insert3 = insertDepoimento(depoimento3, randomNumber);
-
-depoimentosLength = depoimentosList.length;randomNumber = random(depoimentosLength);
-var insert4 = insertDepoimento(depoimento4, randomNumber);
