af6e16b8ca70c4f8a671d77712bda4afb0173e9a
Gabriel <grl.loch@gmail.com>
2019-06-28 17:19:04 -0300

    a

--- scripts/dev.js
+++ scripts/dev.js
@@ -22,57 +22,26 @@ var idToRemove = [
     'OLDCODE5'
 ];
 
+var debug = true;
+
 for (i = 0; urlBlacklist.length > i; i++) {
     if (url.match(urlBlacklist[i])) {
 
-        //console.log("Url encontrada: "); console.log(urlBlacklist[i]);
+        if (debug){console.log("Url encontrada: "); console.log(urlBlacklist[i]);}
 
         for (a = 0; idToRemove.length > a; a++) {
             var stylesheet = document.getElementById(idToRemove[a]);
 
-            // console.log("verificando elemento a ser deletado: "); console.log(stylesheet);
+             if (debug){console.log("verificando elemento a ser deletado: "); console.log(stylesheet);}
 
             if (stylesheet != null) {
-                //console.log("stylesheet != null");
-
-                //console.log("Verificando ID proibida"); console.log(idToRemove[a]);
+                if (debug){console.log("stylesheet != null");console.log("Verificando ID proibida"); console.log(idToRemove[a]);}
 
                 stylesheet.parentNode.removeChild(stylesheet);
 
             }
-            //else {console.log("stylesheet = null");}
+            if (debug){console.log("stylesheet = null");}
         }
         i = urlBlacklist.length + 1;
-    } else {
-    }
-}
-
-
-
-for (i = 0; urlDevList.length > i; i++) {
-    
-    if (url.match(urlDevList[i])) {console.log("Url permitida")}
-    else{
-
-        //console.log("Url encontrada: "); console.log(urlBlacklist[i]);
-
-        for (a = 0; devId.length > a; a++) {
-            var stylesheet = document.getElementById(devId[a]);
-
-            // console.log("verificando elemento a ser deletado: "); console.log(stylesheet);
-
-            if (stylesheet != null) {
-                //console.log("stylesheet != null");
-
-                //console.log("Verificando ID proibida"); console.log(idToRemove[a]);
-
-                stylesheet.parentNode.removeChild(stylesheet);
-                console.log("css removido")
-
-            }
-            //else {console.log("stylesheet = null");}
-        } 
-        i = urlDevList.length + 1; 
-    } else {
-    }
+    } else {}
 }
