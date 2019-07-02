abb44f032d1f1bb7b2fad8a4c64d7f0e53cc9362
Gabriel <grl.loch@gmail.com>
2019-07-02 13:16:42 -0300

    a

--- prendasdolar.com.br/script/carousel/V0002.js
+++ prendasdolar.com.br/script/carousel/V0002.js
@@ -1,8 +1,3 @@
-
-var autoPlayDepoimentos = 10500;
-//var autoPlayDepoimentos = 3500;
-
-
 var carouselHome = new Flickity(document.querySelector('.carousel-home'), {
     draggable: '>1',
     lazyLoad: true,
@@ -16,5 +11,5 @@ var carouselHome = new Flickity(document.querySelector('.carousel-home'), {
 var carouselCurso = new Flickity(document.querySelector('section.header-curso .carousel-header'), {
     draggable: false,
     wrapAround: true,
-    prevNextButtons: false,
+    //prevNextButtons: false,
 });
