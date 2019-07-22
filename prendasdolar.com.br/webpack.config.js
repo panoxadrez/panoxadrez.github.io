b5b863916244021eb3122cdb101cb73e708739be
Gabriel <grl.loch@gmail.com>
2019-07-22 11:38:52 -0300

    5

--- prendasdolar.com.br/webpack.config.js
+++ prendasdolar.com.br/webpack.config.js
@@ -1,16 +1,19 @@
 module.exports = {
     entry: {
-        teste: './script/teste.js'
+        teste: "./script/main.js",
+        //carousel: "./script/carousel/V0002.js"
     },
     output: {
-      path: "./script/",
-      filename: 'bundle.js'  
+      path: __dirname,
+      //path: "./script",
+      publicPath: "/script/",
+      filename: "bundle.js"
     },
     module: {
         rules: [
             {
-                test: /.js$/,
-                exclude: /node_modules/,
+                //test: /.js$/,
+                //exclude: /node_modules/,
                 use:  {
                     loader: 'babel-loader',
                 }
