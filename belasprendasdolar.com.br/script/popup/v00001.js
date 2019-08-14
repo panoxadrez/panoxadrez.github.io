88bdf95190b330f5a5de2858f3a5d97105e8220b
panoxadrez <44067928+panoxadrez@users.noreply.github.com>
2019-08-14 11:18:05 -0300

    54

---
+++ belasprendasdolar.com.br/script/popup/v00001.js
@@ -0,0 +1,3 @@
+function openPopupshare(url, width, height) {
+    var myWindow = window.open("https://www.facebook.com/sharer/sharer.php?u="+"https://" + url, '', "width=" + width + ",height=" + height);
+}
