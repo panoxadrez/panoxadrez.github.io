ac3a191d40d0c55c0edca52e4d7f6aaf5b8a8de1
panoxadrez <44067928+panoxadrez@users.noreply.github.com>
2019-08-14 12:08:34 -0300

    a

--- belasprendasdolar.com.br/script/popup/v00001.js
+++ belasprendasdolar.com.br/script/popup/v00001.js
@@ -10,10 +10,18 @@ function openPopupShare(media, url, shareText, width, height) {
             share = "https://api.whatsapp.com/send?text=" + shareText + " " + url;
             //
             break;
-        default:
-            share = "https://www.facebook.com/sharer/sharer.php?u=" + "https://" + url;
-    }
+        case 'twitter':
+            share = "https://twitter.com/intent/tweet?text=" + shareText + " " + url;
+            break;
 
+        case 'telegram':
+            share = "https://telegram.me/share/url?url=https://" + url + "&amp&" + "text=" + shareText;
+            //&amp;text=Participe comigo das aulas especiais e GRATUITAS de Patchwork e Costura Criativa com Eliana Zerbinatti!
+            break;
+
+        default:
+            console.log("share erro!");
+    }
     console.log(share);
 
     var myWindow = window.open(share, '', "width=" + width + ",height=" + height);
