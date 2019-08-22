f9fd1296066e31c588fd659954b23988c67d99cd
panoxadrez <44067928+panoxadrez@users.noreply.github.com>
2019-08-22 18:32:46 -0300

    a

--- prendasdolar.com.br/script/lib/callFacebookPixel/v000001.js
+++ prendasdolar.com.br/script/lib/callFacebookPixel/v000001.js
@@ -1,9 +1,45 @@
-function callFacebookPixel(i) {
-    switch (i) {
-        case 20:
-            fbq('track', 'Lead');
-            console.log('Facebook pixel called: track Lead')
+function callFacebookPixel(a, b, c) {
+    switch (a) {
+        case 1:
+            a = 'track';
             break;
-        default: console.log('Facebook pixel error, called ' + i + ", and not founded");
+        default: console.log('Facebook pixel error, called ' + a + ", and not founded");
     }
+
+    switch (b) {
+        case 1:
+            b = 'ViewContent'
+            break;
+
+        case 2:
+            b = 'SocialNetwork'
+            break;
+
+        case 3:
+            b = 'LojaVirtual'
+            break;
+
+        case 4:
+            b = 'Lead'
+            break;
+
+        default: console.log('Facebook pixel error, called ' + b + ", and not founded");
+    }
+    switch (c) {
+        case 1:
+            c = '-QuiltInBox'
+            break;
+        case 2:
+            c = 'Cancel'
+            break;
+        default: c = '';
+    }
+    console.log("a = " + a);
+    console.log("b = " + b);
+    console.log("c = " + c);
+
+    b = b + c;
+    console.log("b = " + b);
+
+    fbq(a, b);
 }
