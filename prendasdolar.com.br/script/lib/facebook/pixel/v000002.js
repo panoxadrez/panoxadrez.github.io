9030b851456b87599f3dc1d5a898bfff92a0f3e0
panoxadrez <44067928+panoxadrez@users.noreply.github.com>
2019-08-22 17:43:20 -0300

    si

--- prendasdolar.com.br/script/lib/facebook/pixel/v000002.js
+++ prendasdolar.com.br/script/lib/facebook/pixel/v000002.js
@@ -28,6 +28,7 @@ function facebookPixel(i) {
     switch (i) {
         case 20:
             fbq('track', 'Lead');
+            console.log('Facebook pixel called: track Lead')
             break;
         default: console.log('Facebook pixel error, called ' + i + ", and not founded");
     }
