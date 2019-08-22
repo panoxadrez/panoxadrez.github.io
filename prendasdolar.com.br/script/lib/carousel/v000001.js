3d6a1701de7766ef172cfcb16dca9d3abf22d55b
panoxadrez <44067928+panoxadrez@users.noreply.github.com>
2019-08-22 15:49:59 -0300

    a

---
+++ prendasdolar.com.br/script/lib/carousel/v000001.js
@@ -0,0 +1,8 @@
+var carouselHeaderHome = new Flickity(document.querySelector('.carousel-home'), {
+    draggable: '>1',
+    autoPlay: 15000,
+    wrapAround: true,
+    prevNextButtons: false,
+    cellSelector: '.carousel-cell',
+    initialIndex: 1
+});
---
+++ prendasdolar.com.br/script/lib/depoimentos/v000001.js
@@ -0,0 +1,126 @@
+    var depoimentosList = [
+    [
+        'Nanda Watts', 
+        'Curso de Criação', 
+        'https://panoxadrez.github.io/prendasdolar.com.br/img/paginas/home/depoimentos/nanda_watts.jpg',
+        'Corri para me inscrever no curso e quando as aulas começaram minhas expectativas foram superadas. Com calma, ética e muito bom humor a Tia Lili cativa do começo ao fim. E a vantagem de ter as aulas gravadas é que faço o meu horário, sem pressa nem ansiedade. Apoio, aprovo e recomendo!'
+    ],
+    [
+        'Maria Estela',
+        'Curso de Criação',
+        'https://panoxadrez.github.io/prendasdolar.com.br/img/paginas/home/depoimentos/maria_estela.jpg',
+        'O curso está me propiciando uma visão diferenciada do trabalho com patchwork; venho conseguindo elaborar trabalhos utilizando as técnicas ensinadas pela Tia Lili com muito mais facilidade! O trabalho com blocos tornou-se muito mais prazeroso e apaixonante!!!'
+    ],
+    [
+        'Elisia Barros',
+        'Curso de Criação',
+        'https://panoxadrez.github.io/prendasdolar.com.br/img/paginas/home/depoimentos/elisia_barros.jpg',
+        'A cada retiro anunciado eu ficava <img src="https://panoxadrez.github.io/img/icones/emoji/emoji_apaixonado.png">. Sonhava com a oportunidade de participar em um. Este curso foi exatamente o que desejei por tanto tempo. Estou amando! A plataforma é super fácil de acessar.'
+    ],
+    [
+        'Marilia Goulart',
+        'Curso de Criação',
+        'https://panoxadrez.github.io/prendasdolar.com.br/img/paginas/home/depoimentos/marilia_goulart.jpg',
+        'Tem sido uma bênção, aguardo as aulas ansiosa como uma menina. É meu recomeço depois do tratamento do câncer de mama. Tudo bem explicado, claro e fácil de navegar. Este curso é dez!'
+    ],
+    [
+        'Nina Dias',
+        'Curso de Criação',
+        'https://panoxadrez.github.io/prendasdolar.com.br/img/paginas/home/depoimentos/nina_dias.jpg',
+        'É uma oportunidade de ter a Tia Lili nos orientando e ensinando sobre o Patchwork. Cabe a nós a aplicação de tudo o que assistimos. É muita informação. Eu vejo várias vezes os videos e vou descobrindo sempre um detalhe a mais. Estou amando!!'
+    ],
+    [
+        'Margarete Hoehne',
+        'Curso de Criação',
+        'https://panoxadrez.github.io/prendasdolar.com.br/img/paginas/home/depoimentos/margarete_hoehne.jpg',
+        'Tem sido maravilhoso pra mim que moro longe e principalmente pra me encorajar a criar e acreditar que todas nós conseguimos fazer patchwork. Eliana Zerbinatti com sua experiência e delicadeza em ensinar só tem me ajudado, e a plataforma do curso é super fácil e interativa.'
+    ],
+    [
+        'Ana Carmona',
+        'Curso de Criação',
+        'https://panoxadrez.github.io/prendasdolar.com.br/img/paginas/home/depoimentos/ana_maria_carmona.jpg',
+        'Fiz um retiro com a Tia Lili em Teresópolis em 2105. Claro que me encantei com sua didática e delicadeza! Saí do retiro com outra visão do patchwork. Investi no Quilt in Box e esse curso on line, ah... o que dizer? A cada aula fico mais convencida de que fiz a coisa certa quando me inscrevi!'
+    ],
+    [
+        'Denise Mariano',
+        'Curso de Criação',
+        'https://panoxadrez.github.io/prendasdolar.com.br/img/paginas/home/depoimentos/denise_mariano.jpg',
+        'Tia Lili é uma pessoa fantástica, com uma capacidade ímpar para nos ensinar. É calma e tranquila, o que faz com que nos sintamos à vontade para expormos nossas dúvidas. Estou amando fazer parte da Turma Beta!'
+    ],
+    [
+        'Sandra Mesquita',
+        'Curso de Criação',
+        'https://panoxadrez.github.io/prendasdolar.com.br/img/paginas/home/depoimentos/sandra_mesquita.jpg',
+        'Este curso é um sonho, estou aprendendo muito... ter a oportunidade de aprender com a Tia Lili está sendo fantástico! <img src="https://panoxadrez.github.io/img/icones/emoji/emoji-coracao-vermelho.png"><img src="https://panoxadrez.github.io/img/icones/emoji/emoji-coracao-vermelho.png"><img src="https://panoxadrez.github.io/img/icones/emoji/emoji-coracao-vermelho.png">'
+    ],
+    [
+        'Alessandra Lacerda',
+        'Curso de Criação',
+        'https://panoxadrez.github.io/prendasdolar.com.br/img/paginas/home/depoimentos/alessandra_lacerda.jpg',
+        'Comecei a assistir aos vídeos no YouTube da Tia Lili quando morava no Acre e morria de inveja dos encontros e das turmas presenciais. Quando liguei pra assinar o Quilt in Box e me informaram que ia começar uma turma online eu ADOREI!!!!!'
+    ],
+    [
+        'Gabriela Sarubbi',
+        'Curso de Criação',
+        'https://panoxadrez.github.io/prendasdolar.com.br/img/paginas/home/depoimentos/gabriela_sarubbi.jpg',
+        'O curso de formação de professores da tia Lili é maravilhoso. Tenho aprendido coisas incríveis. A plataform é boa e muito fácil de trabalhar e as aulas são bastante didáticas. Eu recomendo!'
+    ],
+    [
+        'Simone Lourenço',
+        'Curso de Criação',
+        'https://panoxadrez.github.io/prendasdolar.com.br/img/paginas/home/depoimentos/simone_louren%C3%A7o.jpg',
+        'Há mais de 10 anos fiz aula presencial com a Tia Lili, desde então sou apaixonada pela arte e por ela. Assim que soube do curso, logo me inscrevi.'
+    ],
+    [
+        'Elaine Carrascoza',
+        'Curso de Criação',
+        'https://panoxadrez.github.io/prendasdolar.com.br/img/paginas/home/depoimentos/elaine_carrascoza.jpg',
+        'Estou encantada! Já imaginava que seria muito bom, pois sou uma fã incondicional da Tia Lili, já que tudo que sei de patchwork aprendi com suas aulas no YouTube. Quero agradecer pela oportunidade e espero continuar a fazer outros cursos!'
+    ],
+    [
+        'Marlise Galle',
+        'Curso de Criação',
+        'https://panoxadrez.github.io/prendasdolar.com.br/img/paginas/home/depoimentos/marlise_galle.jpg',
+        'A didática da Tia Lili, o carinho e paciência com que ensina numa plataforma fácil de usar e se comunicar, não tem coisa melhor! O nome já diz tudo. Parabéns Eliana Zerbinatti por nos proporcionar tantos momentos de aprendizado e prazer!'
+    ],
+    [
+        'Janira Junqueira',
+        'Curso de Criação',
+        'https://panoxadrez.github.io/prendasdolar.com.br/img/paginas/home/depoimentos/janira_junqueira.jpg',
+        'Estou muito satisfeita com o curso. Desenvolvendo meus trabalhos mesmo lentamente. Tia Lili, é um sonho se realizando. Agradeço a todos!'
+    ]    
+];
+
+var depoimentosLength = depoimentosList.length;
+
+
+function random(number) {
+    return Math.floor(Math.random(number) * depoimentosLength);
+}
+
+function selecionarDepoimento(length) {
+    var i = 0;
+    var counter = 1;
+    while (i <= 0) {
+        var randomNumber = random(length);
+        console.log("random number"+randomNumber + " / loop " + counter);
+        if (depoimentosList[randomNumber] != null) {
+            i = 1
+        }
+        counter++;
+    }
+    return randomNumber;
+}
+function insertDepoimento(classe, position) {
+    document.querySelector(".depoimentos " + classe + " .foto").innerHTML = "<img src='" + depoimentosList[position][2] + "'>";
+    document.querySelector(".depoimentos " + classe + " .nome").innerHTML = "<h5>" + depoimentosList[position][0] + "</h5> <span>" + depoimentosList[position][1] + "</span>";
+    document.querySelector(".depoimentos " + classe + " .depoimento").innerHTML = "<p>" + depoimentosList[position][3] + "</p>";
+    delete depoimentosList[position];
+}
+
+
+var insert1 = insertDepoimento(".depoimento1", selecionarDepoimento());
+var insert2 = insertDepoimento(".depoimento2", selecionarDepoimento());
+var insert3 = insertDepoimento(".depoimento3", selecionarDepoimento());
+var insert4 = insertDepoimento(".depoimento4", selecionarDepoimento());
+
