55b2b3fb9c28d04a4ffa3c81b3b15b802b565aaa
panoxadrez <44067928+panoxadrez@users.noreply.github.com>
2019-08-22 15:46:22 -0300

    a

--- prendasdolar.com.br/script/teste.js
+++
@@ -1,7 +0,0 @@
-function scrollTo(anchor, timeDelay = 1000) {
-    setTimeout(
-            function(){
-                window.location.hash = '#' + anchor;
-            },
-            timeDelay);
-}
--- prendasdolar.com.br/yarn-build.bat
+++
@@ -1,5 +0,0 @@
-yarn add @babel/cli -D
-yarn add @babel/preset-env -D
-yarn add @babel/core -D
-yarn add webpack webpack-cli -D
-yarn add babel-loader -D
