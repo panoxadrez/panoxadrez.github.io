3ac9be2a7516c189bdc4228f2c13f9d25516fcc5
panoxadrez <44067928+panoxadrez@users.noreply.github.com>
2019-08-22 19:30:50 -0300

    a

--- prendasdolar.com.br/script/rc.js
+++ prendasdolar.com.br/script/rc.js
@@ -9,5 +9,6 @@
 
 
 // @koala-append "../lib/bootstrap/4.3.1/js/bootstrap.js"
+
 // @koala-append "../../lib/jquery/jquery-3.4.1.min.js"
 // @koala-append "../../lib/flickity/v2/js/flickity.pkgd.js"
