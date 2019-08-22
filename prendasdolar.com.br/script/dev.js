08f2ec7f471b9f5b8c6078ba5affe49f864a0033
panoxadrez <44067928+panoxadrez@users.noreply.github.com>
2019-08-22 16:54:08 -0300

    a

--- prendasdolar.com.br/script/dev.js
+++ prendasdolar.com.br/script/dev.js
@@ -1,5 +1,6 @@
-// @koala-append "../lib/flickity/v2/js/flickity.pkgd.js"
+// @koala-append "../lib/jquery/jquery-3.4.1.min.js"
 // @koala-append "../lib/bootstrap/4.3.1/js/bootstrap.js"
+// @koala-append "../lib/flickity/v2/js/flickity.pkgd.js"
 // @koala-append "lib/carousel/v000002.js"
 // @koala-append "lib/depoimentos/v000001.js"
 // @koala-append "lib/facebook/pixel/v000001.js"
