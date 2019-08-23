626ebdaa8689bdc2e5a946954fcb7a3e2fbd0824
panoxadrez <44067928+panoxadrez@users.noreply.github.com>
2019-08-23 16:47:58 -0300

    a

--- prendasdolar.com.br/script/rc.js
+++ prendasdolar.com.br/script/rc.js
@@ -1,3 +1,5 @@
+// @koala-append "lib/leadlovers/capture_form/v000001.js"
+
 // @koala-append "lib/facebook/pixel/v000001.js"
 // @koala-append "../../lib/jquery/jquery-3.4.1.min.js"
 // @koala-append "../../lib/heatmap/v00001.js"
@@ -6,5 +8,4 @@
 // @koala-append "lib/carousel/v000002.js"
 // @koala-append "lib/depoimentos/v000001.js"
 // @koala-append "lib/float_comprar/v000001.js"
-// @koala-append "lib/leadlovers/capture_form/v000001.js"
 // @koala-append "lib/callFacebookPixel/v000001.js"
