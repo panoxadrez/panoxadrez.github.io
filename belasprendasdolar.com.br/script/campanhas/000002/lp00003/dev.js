b4e289c64d24a31a8ede6d6d590c50dca63d19da
panoxadrez <44067928+panoxadrez@users.noreply.github.com>
2019-08-30 10:39:52 -0300

    a

--- belasprendasdolar.com.br/script/campanhas/000002/lp00003/dev.js
+++ belasprendasdolar.com.br/script/campanhas/000002/lp00003/dev.js
@@ -1,4 +1,4 @@
-// @koala-append "../../lib/popup/v00001.js"
-// @koala-append "../../lib/heatmap/v00001.js"
-// @koala-append "../../lib/leadlovers/form-capture/v1.js"
+// @koala-append "../../../lib/popup/v00001.js"
+// @koala-append "../../../lib/heatmap/v00001.js"
+// @koala-append "../../../lib/leadlovers/form-capture/v1.js"
 // @koala-append "lib/facebook/pixel/v1.js"
