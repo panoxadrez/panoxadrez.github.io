0ad1290c7eb751867b5270005c34293ca7d3938b
panoxadrez <44067928+panoxadrez@users.noreply.github.com>
2019-08-30 12:02:38 -0300

    a

--- belasprendasdolar.com.br/script/campanhas/000002/aulas/dev.js
+++ belasprendasdolar.com.br/script/campanhas/000002/aulas/dev.js
@@ -8,3 +8,4 @@
 // @koala-append "../../../lib/heatmap/v00001.js"
 // @koala-append "../../../lib/leadlovers/form-capture/v1.js"
 // @koala-append "lib/facebook/pixel/v1.js"
+// @koala-append "lib/carousel-config/v00001.js"
---
+++ belasprendasdolar.com.br/script/campanhas/000002/aulas/lib/carousel-config/v00001.js
@@ -0,0 +1,11 @@
+var carouselHome = new Flickity(document.querySelector('.carousel-container .carousel'), {
+    draggable: false,
+    freeScroll: false,
+    prevNextButtons: false,
+    pageDots: false,
+    wrapAround: true,
+    autoPlay: false,
+    fullscreen: true,
+    fade: true,
+    hash: true
+});
