a9335a7b23438ab755eb1ab717db61cd10005917
panoxadrez <44067928+panoxadrez@users.noreply.github.com>
2019-08-30 11:52:14 -0300

    d

--- belasprendasdolar.com.br/script/campanhas/000002/aulas/dev.js
+++ belasprendasdolar.com.br/script/campanhas/000002/aulas/dev.js
@@ -1,3 +1,9 @@
+//lib
+// @koala-append "../../../../../lib/flickity/v2/js/flickity.pkgd.js"
+// @koala-append "../../../../../lib/jquery/jquery-3.4.1.min.js"
+// @koala-append "../../../../../lib/popper/popper.min.js"
+// @koala-append "../../../../../lib/bootstrap/4.3.1/js/bootstrap.bundle.min.js"
+
 // @koala-append "../../../lib/popup/v00001.js"
 // @koala-append "../../../lib/heatmap/v00001.js"
 // @koala-append "../../../lib/leadlovers/form-capture/v1.js"
