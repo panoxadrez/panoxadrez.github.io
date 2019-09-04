ab72e99eee3bdcf71f772191e6bdd20402f820f0
panoxadrez <44067928+panoxadrez@users.noreply.github.com>
2019-09-04 09:41:08 -0300

    5

--- belasprendasdolar.com.br/script/campanhas/000002/aulas/dev.js
+++ belasprendasdolar.com.br/script/campanhas/000002/aulas/dev.js
@@ -1,6 +1,6 @@
 //lib
 // @koala-append "../../../../../lib/flickity/v2/js/flickity.pkgd.js"
-// @koala-append "../../../../../../../../flickity-hash.git/hash.js"
+// @koala-append "../../../../../../../flickity-hash.git/hash.js"
 // @koala-append "../../../../../lib/jquery/jquery-3.4.1.min.js"
 // @koala-append "../../../../../lib/popper/popper.min.js"
 // @koala-append "../../../../../lib/bootstrap/4.3.1/js/bootstrap.bundle.min.js"
@@ -9,5 +9,3 @@
 // @koala-append "../../../lib/leadlovers/form-capture/v1.js"
 // @koala-append "lib/facebook/pixel/v1.js"
 // @koala-append "lib/carousel-config/v00001.js"
-
-// @koala-append "../../../../../../../../sushi-framework/script/jumpto/v00001.js"
