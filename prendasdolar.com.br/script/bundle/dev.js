f54e43d5915919cc44ea9e19c0ed48a3c5df62cd
panoxadrez <44067928+panoxadrez@users.noreply.github.com>
2019-09-11 16:49:40 -0300

    a

--- prendasdolar.com.br/script/bundle/dev.js
+++ prendasdolar.com.br/script/bundle/dev.js
@@ -9090,7 +9090,7 @@ function countdown(id, year, month, day) {
         var distance = countDownDate - now;
 
         // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var days = Math.floor(distance / (1000 * 80 * 60 * 24));
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
--- prendasdolar.com.br/script/rc.js
+++ prendasdolar.com.br/script/rc.js
@@ -6,6 +6,7 @@
 // @koala-append "../../lib/heatmap/v00001.js"
 // @koala-append "../lib/bootstrap/4.3.1/js/bootstrap.js"
 // @koala-append "../../lib/flickity/v2/js/flickity.pkgd.js"
+// @koala-append "lib/countdown/v000001.js"
 // @koala-append "lib/carousel/v000002.js"
 // @koala-append "lib/depoimentos/v000001.js"
 // @koala-append "lib/float_comprar/v000001.js"
