9f3e28905e73be07f1fc4e1ab063f43216aebf37
panoxadrez <44067928+panoxadrez@users.noreply.github.com>
2019-09-24 16:07:59 -0300

    a

--- prendasdolar.com.br/script/lib/libs/v000001.js
+++ prendasdolar.com.br/script/lib/libs/v000001.js
@@ -1,3 +1,2 @@
-// @koala-append "../../../../lib/flickity/v2/js/flickity.pkgd.js"
-// @koala-append "../../../../lib/jquery/jquery-3.4.1.js"
+// @koala-append "../../../../lib/jquery/jquery-3.4.1.min.js"
 // @koala-append "../../../lib/bootstrap/4.3.1/js/bootstrap.js"
--- prendasdolar.com.br/script/live.js
+++ prendasdolar.com.br/script/live.js
@@ -1,16 +1,12 @@
 //live
 
-// @koala-append "lib/facebook/pixel/v000001.js"
-// @koala-append "../../lib/jquery/jquery-3.4.1.min.js"
-// @koala-append "../../lib/heatmap/v00001.js"
-// @koala-append "../lib/bootstrap/4.3.1/js/bootstrap.js"
-// @koala-append "../../lib/flickity/v2/js/flickity.pkgd.js"
-// @koala-append "lib/countdown/v000002.js"
-// @koala-append "lib/carousel/v000002.js"
-// @koala-append "lib/depoimentos/v000001.js"
-// @koala-append "lib/float_comprar/v000001.js"
-// @koala-append "lib/leadlovers/capture_form/v000001.js"
-// @koala-append "lib/callFacebookPixel/v000001.js"
+// @koala-prepend "../../lib/flickity/v2/js/flickity.pkgd.js"
+// @koala-prepend "lib/libs/v000001.js"
+// @koala-prepend "lib/facebook/pixel/v000001.js"
+
+
+
+
 
 
 
