ccabf2b55340e4e13fe93c95770ad63b8c19cba3
panoxadrez <44067928+panoxadrez@users.noreply.github.com>
2019-09-24 17:50:44 -0300

    a

--- prendasdolar.com.br/script/live.js
+++ prendasdolar.com.br/script/live.js
@@ -1,17 +1,14 @@
-//live
+//dev
 
-// @koala-prepend "../../lib/flickity/v2/js/flickity.pkgd.js"
 // @koala-prepend "lib/libs/v000001.js"
 // @koala-prepend "lib/facebook/pixel/v000001.js"
 
+// @koala-prepend "../../lib/heatmap/v00001.js"
+// @koala-prepend "lib/callFacebookPixel/v000001.js"
+// @koala-prepend "lib/leadlovers/capture_form/v000001.js"
 
-
-
-
-
-
-
-
-
-
-countdown('#curso00002', '.pagina-venda-header', ' .cta-carimbo');
+// @koala-prepend "lib/cursoMudaStatus/v000001.js"
+// @koala-prepend "lib/countdown/v000002.js"
+// @koala-prepend "lib/carousel/v000002.js"
+// @koala-prepend "lib/depoimentos/v000001.js"
+// @koala-prepend "lib/float_comprar/v000001.js"
