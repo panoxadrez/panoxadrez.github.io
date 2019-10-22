373b34930be394c209855a2afc0d882b7b219ad0
panoxadrez <44067928+panoxadrez@users.noreply.github.com>
2019-10-22 17:21:37 -0300

    a

--- quiltinbox.com.br/script/dev.js
+++ quiltinbox.com.br/script/dev.js
@@ -5,5 +5,8 @@
 // @koala-prepend  "../../lib/bootstrap/4.3.1/js/bootstrap.bundle.min.js"
 // @koala-prepend  "../../lib/flickity/v2/js/flickity.pkgd.js"
 
-// @koala-prepend  "lib/leadlovers/form-capture/v1.js"
-// @koala-prepend  "lib/facebook/pixel/v1.js"
+// @koala-prepend  "../../lib/leadlovers/form-capture/v2.js"
+// @koala-prepend  "../../lib/facebook/pixel/v1.js"
+
+fbq('init', '408507799815686');
+fbq('track', 'PageView');
