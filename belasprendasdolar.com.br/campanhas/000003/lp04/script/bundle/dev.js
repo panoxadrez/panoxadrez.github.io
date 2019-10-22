5df60c9dd6ac82e45a34935b26cdd3b1a8deae1f
panoxadrez <44067928+panoxadrez@users.noreply.github.com>
2019-10-22 17:06:39 -0300

    fg

--- belasprendasdolar.com.br/campanhas/000003/lp04/script/bundle/dev.js
+++ belasprendasdolar.com.br/campanhas/000003/lp04/script/bundle/dev.js
@@ -4741,184 +4741,187 @@ function openPopupShare(media, url, shareText, width, height) {
 (function () {
 
 	"use strict";
-  
+
 	window.onload = function () {
-	    LLEvents();
+		LLEvents();
 	};
-  
+
 	var currentForm = null;
-  
+
 	function LLCapture(objForm) {
-	    if (!objForm) { console.error('Form not found!'); return; }
-	    if (!objForm.action) { console.error('Form without action'); return; }
-	    currentForm = objForm;
-	    var request = new XMLHttpRequest();
-	    request.onload = LLCaptureReturn;
-	    if (objForm.method.toLowerCase() === 'post') {
-		  LLCaptureClearErrors(objForm);
-		  if (LLCaptureValidations(objForm)) {
-			LLToogleFields(objForm, false);
-			LLToogleLoading(objForm, true);
-			request.open("post", objForm.action, true);
-			request.send(new FormData(objForm));
-			fbq('track', 'CompleteRegistration');
-		  }
-	    }
-	    return false;
+		if (!objForm) { console.error('Form not found!'); return; }
+		if (!objForm.action) { console.error('Form without action'); return; }
+		currentForm = objForm;
+		var request = new XMLHttpRequest();
+		request.onload = LLCaptureReturn;
+		if (objForm.method.toLowerCase() === 'post') {
+			LLCaptureClearErrors(objForm);
+			if (LLCaptureValidations(objForm)) {
+				LLToogleFields(objForm, false);
+				LLToogleLoading(objForm, true);
+				request.open("post", objForm.action, true);
+				request.send(new FormData(objForm));
+				fbq('track', 'CompleteRegistration', {
+					value: 1,
+					currency: 'brl',
+				});
+			}
+		}
+		return false;
 	}
 	function LLCaptureClearErrors(objForm) {
-	    if (!!objForm) {
-		  var elements = objForm.querySelectorAll('div.field-error');
-		  if (elements.length > 0) {
-			elements.forEach(function (item, ix) {
-			    item.innerText = '';
-			    item.className = item.className.replace('show', '');
-			});
-		  }
-	    }
-	    else
-		  console.error('Form not found!');
+		if (!!objForm) {
+			var elements = objForm.querySelectorAll('div.field-error');
+			if (elements.length > 0) {
+				elements.forEach(function (item, ix) {
+					item.innerText = '';
+					item.className = item.className.replace('show', '');
+				});
+			}
+		}
+		else
+			console.error('Form not found!');
 	}
 	function LLCaptureReturn() {
-	    if (!!currentForm) {
-		  var objForm = currentForm;
-		  var errorBox = objForm.querySelector('div.error-box');
-		  var errorDiv = errorBox.querySelector('div');
-		  var div = null;
-		  var error = false;
-		  var result = JSON.parse(this.responseText);
-		  if (!!result.errors) {
-			result.errors.forEach(function (err, ix) {
-			    if (err.field == null || err.field === '') {
-				  error = true;
-				  errorDiv.innerHTML += '<span>' + err.error + '</span><br />';
-			    }
-			    else {
-				  div = objForm.querySelector(err.field);
-				  if (!!div) {
-					div.innerHTML = err.error;
-					if (div.className.indexOf('show') === -1)
-					    div.className += ' show';
-				  }
-			    }
-			});
-			LLToogleLoading(objForm, false);
-			if (error)
-			    LLToogleError(objForm, true);
+		if (!!currentForm) {
+			var objForm = currentForm;
+			var errorBox = objForm.querySelector('div.error-box');
+			var errorDiv = errorBox.querySelector('div');
+			var div = null;
+			var error = false;
+			var result = JSON.parse(this.responseText);
+			if (!!result.errors) {
+				result.errors.forEach(function (err, ix) {
+					if (err.field == null || err.field === '') {
+						error = true;
+						errorDiv.innerHTML += '<span>' + err.error + '</span><br />';
+					}
+					else {
+						div = objForm.querySelector(err.field);
+						if (!!div) {
+							div.innerHTML = err.error;
+							if (div.className.indexOf('show') === -1)
+								div.className += ' show';
+						}
+					}
+				});
+				LLToogleLoading(objForm, false);
+				if (error)
+					LLToogleError(objForm, true);
+				else
+					LLToogleFields(objForm, true);
+			}
 			else
-			    LLToogleFields(objForm, true);
-		  }
-		  else
-			window.location.href = result;
-	    }
-	    else
-		  console.error('Form not found!');
+				window.location.href = result;
+		}
+		else
+			console.error('Form not found!');
 	}
 	function LLCaptureValidations(objForm) {
-	    var err = null;
-	    var elements = null;
-	    var result = true;
-	    // get required elements
-	    elements = objForm.querySelectorAll('[data-required]');
-	    if (elements.length > 0) {
-		  elements.forEach(function (item, ix) {
-			if (item.tagName.toLowerCase() !== 'div') {
-			    if (item.value.trim() === '') {
-				  result = false;
-				  err = item.parentElement.querySelector('div.field-error');
-				  if (err) {
-					err.innerText = 'Esse campo é obrigatório';
-					err.className += ' show';
-				  }
-			    }
-			}
-			else {
-			    var items = item.querySelectorAll('input[type=checkbox]');
-			    var empty = [].filter.call(items, function (el) {
-				  return !el.checked;
-			    });
-			    if (items.length == empty.length) {
-				  result = false;
-				  err = item.parentElement.querySelector('div.field-error');
-				  if (err) {
-					err.innerText = 'Selecione ao menos um item';
-					err.className += ' show';
-				  }
-			    }
-			}
-		  });
-	    }
-	    // return
-	    return result;
+		var err = null;
+		var elements = null;
+		var result = true;
+		// get required elements
+		elements = objForm.querySelectorAll('[data-required]');
+		if (elements.length > 0) {
+			elements.forEach(function (item, ix) {
+				if (item.tagName.toLowerCase() !== 'div') {
+					if (item.value.trim() === '') {
+						result = false;
+						err = item.parentElement.querySelector('div.field-error');
+						if (err) {
+							err.innerText = 'Esse campo é obrigatório';
+							err.className += ' show';
+						}
+					}
+				}
+				else {
+					var items = item.querySelectorAll('input[type=checkbox]');
+					var empty = [].filter.call(items, function (el) {
+						return !el.checked;
+					});
+					if (items.length == empty.length) {
+						result = false;
+						err = item.parentElement.querySelector('div.field-error');
+						if (err) {
+							err.innerText = 'Selecione ao menos um item';
+							err.className += ' show';
+						}
+					}
+				}
+			});
+		}
+		// return
+		return result;
 	}
 	function LLEvents() {
-	    var found = false;
-	    var forms = document.getElementsByTagName('form');
-	    if (forms.length > 0) {
-		  for (var i = 0; i < forms.length; i++) {
-			if (forms[i] != undefined && forms[i] != null && forms[i].id == 'llCaptureForm') {
-			    found = true;
-			    var objForm = forms[i];                    
-			    objForm.onsubmit = function () {
-				  LLCapture(this);
-				  return false;
-			    };
-			    objForm.querySelector('#btn-error').addEventListener('click', function () {
-				  LLToogleError(this, false);
-				  LLToogleFields(this, true);
-			    });
+		var found = false;
+		var forms = document.getElementsByTagName('form');
+		if (forms.length > 0) {
+			for (var i = 0; i < forms.length; i++) {
+				if (forms[i] != undefined && forms[i] != null && forms[i].id == 'llCaptureForm') {
+					found = true;
+					var objForm = forms[i];
+					objForm.onsubmit = function () {
+						LLCapture(this);
+						return false;
+					};
+					objForm.querySelector('#btn-error').addEventListener('click', function () {
+						LLToogleError(this, false);
+						LLToogleFields(this, true);
+					});
+				}
 			}
-		  }
-	    }
-	    else
-		  console.error('No dynamic form found!');
+		}
+		else
+			console.error('No dynamic form found!');
 	}
 	function LLToogleError(objForm, state) {
-	    if (!!objForm) {
-		  var box = objForm.querySelector('div.error-box');
-		  if (box) {
-			if (state)
-			    box.className += ' show';
-			else
-			    box.className = box.className.replace('show', '');
-		  }
-	    }
-	    else
-		  console.error('Form not found!');
+		if (!!objForm) {
+			var box = objForm.querySelector('div.error-box');
+			if (box) {
+				if (state)
+					box.className += ' show';
+				else
+					box.className = box.className.replace('show', '');
+			}
+		}
+		else
+			console.error('Form not found!');
 	}
 	function LLToogleFields(objForm, state) {
-	    if (!!objForm) {
-		  var children = Array.from(objForm.children);
-		  children.forEach(function (item, ix) {
-			if (item.tagName.toLowerCase() === 'div') {
-			    if (item.className.indexOf('loading-box') === -1 &&
-				  item.className.indexOf('error-box') === -1) {
-				  if (!state)
-					item.className += ' hidden';
-				  else
-					item.className = item.className.replace('hidden', '');
-			    }
-			}
-		  });
-	    }
-	    else
-		  console.error('Form not found!');
+		if (!!objForm) {
+			var children = Array.from(objForm.children);
+			children.forEach(function (item, ix) {
+				if (item.tagName.toLowerCase() === 'div') {
+					if (item.className.indexOf('loading-box') === -1 &&
+						item.className.indexOf('error-box') === -1) {
+						if (!state)
+							item.className += ' hidden';
+						else
+							item.className = item.className.replace('hidden', '');
+					}
+				}
+			});
+		}
+		else
+			console.error('Form not found!');
 	}
 	function LLToogleLoading(objForm, state) {
-	    if (!!objForm) {
-		  var box = objForm.querySelector('div.loading-box');
-		  if (box) {
-			if (state)
-			    box.className += ' show';
-			else
-			    box.className = box.className.replace('show', '');
-		  }
-	    }
-	    else
-		  console.error('Form not found!');
+		if (!!objForm) {
+			var box = objForm.querySelector('div.loading-box');
+			if (box) {
+				if (state)
+					box.className += ' show';
+				else
+					box.className = box.className.replace('show', '');
+			}
+		}
+		else
+			console.error('Form not found!');
 	}
-  
-  }());
+
+}());
 
 ! function (f, b, e, v, n, t, s) {
 	if (f.fbq) return;
@@ -4953,7 +4956,7 @@ fbq('track', 'PageView');
 // @koala-prepend  "../../../../lib/scripts/instagram/getjson/v00001.js"
 
 // @koala-prepend  "../../../../lib/scripts/popup/v00001.js"
-// @koala-prepend  "../../../../lib/scripts/leadlovers/form-capture/v1.js"
+// @koala-prepend  "../../../../lib/scripts/leadlovers/form-capture/v2.js"
 // @koala-prepend  "lib/facebook/pixel/v1.js"
 
 
