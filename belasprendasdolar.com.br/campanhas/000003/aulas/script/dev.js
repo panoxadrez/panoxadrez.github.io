19d4c5fc8cf9bb7f7cbd4e7f1589784055bcf4cf
panoxadrez <44067928+panoxadrez@users.noreply.github.com>
2019-11-01 09:59:50 -0300

    a

--- belasprendasdolar.com.br/campanhas/000003/aulas/script/dev.js
+++ belasprendasdolar.com.br/campanhas/000003/aulas/script/dev.js
@@ -13,10 +13,7 @@
 // @koala-prepend  "lib/facebook/pixel/v1.js"
 // @koala-prepend  "lib/menu_aulas_display/v1.js"
 
-
 dataIgPost();
-
-
 
 //carrossel config
 const carrossel = $('.carousel-container .carousel').flickity({
@@ -33,7 +30,7 @@ const carrossel = $('.carousel-container .carousel').flickity({
 function lba(aulaLista, libDateRaw, slideSelector, tipo) {
     const listaAula = $('.aula-video .lista-aula .aula-' + aulaLista);
     const carrosselCell = $('.carousel-container #carousel-aula-dia-0' + slideSelector);
-    const now = new Date();
+    const now = new Date(2019, 10, 5);
     libDateArray = libDateRaw.split(',');
 
     const libDate = new Date(libDateArray[0], libDateArray[1] - 1, libDateArray[2], libDateArray[3], libDateArray[4]);
@@ -79,12 +76,12 @@ function lba(aulaLista, libDateRaw, slideSelector, tipo) {
 
 }
 lba('01', '2019,11,2,18,00', 1, 'g');
-lba('02', '2019, 11, 3, 20, 0', 2, 'v');
-lba('03', '2019, 11, 4, 18, 0', 3, 'g');
-lba('04', '2019, 11, 5, 21, 0', 4, 'v');
-lba('05', '2019, 11, 6, 18, 0', 5, 'g');
-lba('06', '2019, 11, 7, 21, 0', 6, 'v');
-lba('07', '2019, 11, 8, 18, 0', 7, 'c');
+lba('02', '2019,11,3,20,00', 2, 'v');
+lba('03', '2019,11,4,18,00', 3, 'g');
+lba('04', '2019,11,5,21,00', 4, 'v');
+lba('05', '2019,11,6,18,00', 5, 'g');
+lba('06', '2019,11,7,21,00', 6, 'v');
+lba('07', '2019,11,8,18,00', 7, 'c');
 
 
 //gravado 18 / ao vivo domingo 20 / ao vivo semana 21
