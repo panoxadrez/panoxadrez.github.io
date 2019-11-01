5930241e3f46ccdcc4d3ed33865555971fb6e07f
panoxadrez <44067928+panoxadrez@users.noreply.github.com>
2019-11-01 16:37:26 -0300

    ads

--- belasprendasdolar.com.br/campanhas/000003/aulas/script/bundle/dev.js
+++ belasprendasdolar.com.br/campanhas/000003/aulas/script/bundle/dev.js
@@ -4747,73 +4747,6 @@ return Flickity;
 
 
 dataIgPost();
-
-$(
-    function () {
-        $('.carousel-container #carousel-aula-dia-01').find('img').show().find('iframe').show();
-        $('.carousel-container #carousel-aula-dia-02').find('img').show().find('iframe').show();
-        $('.carousel-container #carousel-aula-dia-03').find('img').show().find('iframe').show();
-        $('.carousel-container #carousel-aula-dia-04').find('img').show().find('iframe').show();
-        $('.carousel-container #carousel-aula-dia-05').find('img').show().find('iframe').show();
-        $('.carousel-container #carousel-aula-dia-06').find('img').show().find('iframe').show();
-        $('.carousel-container #carosel-recado').show();
-    }
-);
-
-function lba(aulaLista, libDateRaw, slideSelector, tipo) {
-    const listaAula = $('.aula-video .lista-aula .aula-' + aulaLista);
-    const carrosselCell = $('.carousel-container #carousel-aula-dia-0' + slideSelector);
-    const now = new Date(2019, 10, 10, 18, 01);
-    libDateArray = libDateRaw.split(',');
-
-    const libDate = new Date(libDateArray[0], libDateArray[1] - 1, libDateArray[2], libDateArray[3], libDateArray[4]);
-
-    //HOJE!
-    if (now.getFullYear() == libDate.getFullYear() && now.getMonth() == libDate.getMonth() && now.getDate() == libDate.getDate()) {
-        listaAula.removeClass('lock').removeClass('amanha').removeClass('liberada');
-        listaAula.addClass('hoje');
-        listaAula.find('.status').html("HOJE!");
-
-
-        if ((libDate - now) <= 0) {
-            removeImg();
-        }
-        if (tipo != 'c') {
-            carrossel.flickity('select', (slideSelector - 1));
-        }
-    }
-    // liberada
-    else if (now.getFullYear() == libDate.getFullYear() && now.getMonth() == libDate.getMonth() && now.getDate() > libDate.getDate()) {
-        listaAula.removeClass('lock').removeClass('amanha').removeClass('hoje');
-        listaAula.addClass('liberada');
-        listaAula.find('.status').html("LIBERADA");
-        removeImg();
-        if (tipo == 'c') {
-            liberaChamada();
-        }
-    }
-    //em breve
-    else {
-        listaAula.removeClass('amanha').removeClass('liberada').removeClass('hoje');
-        listaAula.addClass('lock');
-        listaAula.find('.status').html("EM BREVE!");
-        if (tipo == 'v' || tipo == 'c') {
-            removeImg();
-            $('.carousel-container #carosel-recado').hide();
-        } else {
-            removeIframe();
-        }
-    }
-
-    function removeIframe() { carrosselCell.find('iframe').hide(); }
-    function removeImg() { carrosselCell.find('img').hide(); }
-    function liberaChamada() {
-        $('.link-chamada').addClass('show');
-        carrossel.flickity('select', slideSelector - 1);
-    }
-
-}
-
 //carrossel config
 const carrossel = $('.carousel-container .carousel').flickity({
     draggable: false,
@@ -4826,10 +4759,80 @@ const carrossel = $('.carousel-container .carousel').flickity({
     hash: true
 });
 
-lba('01', '2019,11,2,18,00', 1, 'g');
-lba('02', '2019,11,3,20,00', 2, 'v');
-lba('03', '2019,11,4,18,00', 3, 'g');
-lba('04', '2019,11,5,21,00', 4, 'v');
-lba('05', '2019,11,6,18,00', 5, 'g');
-lba('06', '2019,11,7,21,00', 6, 'v');
-lba('07', '2019,11,8,18,00', 7, 'c');
+$(
+    function () {
+        $('.carousel-container #carousel-aula-dia-01').show('img', 'iframe');
+        $('.carousel-container #carousel-aula-dia-02').show('img', 'iframe');
+        $('.carousel-container #carousel-aula-dia-03').show('img', 'iframe');
+        $('.carousel-container #carousel-aula-dia-04').show('img', 'iframe');
+        $('.carousel-container #carousel-aula-dia-05').show('img', 'iframe');
+        $('.carousel-container #carousel-aula-dia-06').show('img', 'iframe');
+        $('.carousel-container #carosel-recado').show();
+    }
+);
+
+now = new Date();
+function lba(aulaLista, libDateRaw, aulaNum, slide, tipo) {
+
+    const listaAula = $('.aula-video .lista-aula .aula-' + aulaLista);
+    const carrosselCell = $('.carousel-container #carousel-aula-dia-0' + aulaNum);
+    libDateArray = libDateRaw.split(',');
+
+    const libDate = new Date(libDateArray[0], libDateArray[1] - 1, libDateArray[2], libDateArray[3], libDateArray[4]);
+
+    //HOJE!
+    if (now.getFullYear() == libDate.getFullYear() && now.getMonth() == libDate.getMonth() && now.getDate() == libDate.getDate()) {
+        listaAula.removeClass('lock').removeClass('amanha').removeClass('liberada');
+        listaAula.addClass('hoje');
+        listaAula.find('.status').html("HOJE!");
+
+        if ((libDate - now) <= 0) {
+            removeImg();
+        }
+        if (tipo != 'c') {
+            removeChamada()
+            carrossel.flickity('select', slide);
+        }
+    }
+    // liberada
+    else if (now.getFullYear() == libDate.getFullYear() && now.getMonth() == libDate.getMonth() && now.getDate() > libDate.getDate()) {
+        listaAula.removeClass('lock').removeClass('amanha').removeClass('hoje');
+        listaAula.addClass('liberada');
+        listaAula.find('.status').html("LIBERADA");
+        removeImg();
+        if (tipo == 'c') {
+            liberaChamada();
+            carrossel.flickity('select', slide);
+        }
+    }
+    //em breve
+    else {
+        listaAula.removeClass('amanha').removeClass('liberada').removeClass('hoje');
+        listaAula.addClass('lock');
+        listaAula.find('.status').html("EM BREVE!");
+        if (tipo == 'v' || tipo == 'c') {
+            removeImg();
+            removeChamada()
+        } else {
+            removeIframe();
+        }
+    }
+
+    function removeIframe() { carrosselCell.find('iframe').hide(); }
+    function removeImg() { carrosselCell.find('img').hide(); }
+    function removeChamada() {
+        $('.carousel-container #carosel-recado').hide();
+    }
+    function liberaChamada() {
+        $('.link-chamada').addClass('show');
+    }
+
+}
+
+lba('01', '2019,11,2,18,00', 1, 'g', 0);
+lba('02', '2019,11,3,20,00', 2, 'v', 1);
+lba('03', '2019,11,4,18,00', 3, 'g', 2);
+lba('04', '2019,11,5,21,00', 4, 'v', 3);
+lba('05', '2019,11,6,18,00', 5, 'g', 4);
+lba('06', '2019,11,7,21,00', 6, 'v', 5);
+lba('', '2019,11,8,18,00', 7, 'c', 6);
